c4d8c72f147463c182ed79b090439fc40ae452f0
luatluat304@gmail.com <luatluat304@gmail.com>
2026-01-15 16:22:17 +0700

    add util for mongooes

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,21 +1,23 @@
-const Course = require('../models/Course');
-
+const Course = require("../models/Course");
+const { multi } = require("../../util/mongoose.js");
 class SiteController {
-    //GET /home
-    index(req, res) {
-        Course.find({})
-            .then((docs) => {
-                res.json(docs);
-            })
-            .catch((err) => {
-                res.status(400).json({ error: err.message });
-            });
-    }
+  //GET /home
+  index(req, res) {
+    Course.find({})
+      .then((docs) => {
+        docs = multi(docs);
+        //res.json(docs);
+        res.render("home", { courses: docs });
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err.message });
+      });
+  }
 
-    //[get] /search
-    search(req, res) {
-        res.render('search');
-    }
+  //[get] /search
+  search(req, res) {
+    res.render("search");
+  }
 }
 
 module.exports = new SiteController();
---
+++ src/util/mongoose.js.js
@@ -0,0 +1,8 @@
+module.exports = {
+  multipleMongooseToObject: function (monooseArray) {
+    return monooseArray.map((monooseArray) => monooseArray.toObject());
+  },
+  mongooseToOBject(mongooes) {
+    return mongooes ? mongooes.toObject() : mongooes;
+  },
+};
