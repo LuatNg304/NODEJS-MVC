fcab832e205f7984ce107e8327680c1e080e126c
luatluat304@gmail.com <luatluat304@gmail.com>
2026-01-21 13:04:19 +0700

    update course/show

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,10 +1,16 @@
+const { mongooseToOBject } = require("../../util/mongoose.js");
 const Course = require("../models/Course");
 class CourseController {
   //:slug
   show(req, res,next) {
+
+    //qyerry : dung querry.
+    //thongo tin khong qua url thi params
+    //post: thi qua body
     Course.findOne({slug : req.params.slug})
     .then(course =>{
-      res.render('courses/show')
+      res.render('courses/show',{course : mongooseToOBject(course)})
+      
     }).catch(next)
   }
   //:slug
